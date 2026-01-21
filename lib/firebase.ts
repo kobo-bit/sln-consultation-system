816b62c3f4517f08a1af7c5f352d71ccd2228b84
kaito obo <k_obo@n-sln.org>
2026-01-21 09:44:33 +0900

    2026/01/21 09:44(機能修正)

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // 認証機能は後で使います
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,4 +20,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // データベース機能を使えるようにしてエクスポート
 export const db = getFirestore(app);
 export const auth = getAuth(app); // 後で有効化
-
+export const storage = getStorage(app);
